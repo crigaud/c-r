201f93ebcec09770781f5d2042b76f0284b6cf30
crigaud <c.rigaud@gmx.com>
2013-04-21 22:46:19 +0200

    rm calendar

--- calendar/calendars/recur_tests/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//header("HTTP/1.1 301 Moved Permanently");
- 	if(preg_match('/fr/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'])){
-		header("Location: http://". $_SERVER['SERVER_NAME']."/fr/");
-	}else{
- 		header("Location: http://". $_SERVER['SERVER_NAME']."/en/");
-}
-?>
-
